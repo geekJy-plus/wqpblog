6c17e3815e93dff49638b306f3ec1e381b47a380
wangqinpeng <1197763589@qq.com>
2020-05-28 09:51:01 +0800

    配置了PWA

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,7 +8,15 @@ module.exports = {
     head: [
         ['link', { rel: 'icon', href: 'assets/img/favicon.ico' }],
         ['meta', { name: 'author', content: '王钦鹏' }],
-        ['meta', { name: 'keywords', content: 'vuepress ,王钦鹏,wangqinpeng,博客,blog' }]
+        ['meta', { name: 'keywords', content: 'vuepress ,王钦鹏,wangqinpeng,博客,blog' }],
+        ['link', { rel: 'manifest', href: '/manifest.json' }],
+        ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+        ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+        ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+        ['link', { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon-152x152.png' }],
+        ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+        ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+        ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
     ],
     plugins: [
         [
@@ -19,7 +27,14 @@ module.exports = {
                     return moment(timestamp).format("LLLL")
                 }
             }
-        ]
+        ],
+        ['@vuepress/pwa', {
+            serviceWorker: true,
+            updatePopup: {
+                message: "发现新内容",
+                buttonText: "刷新"
+            }
+        }]
     ],
     themeConfig: {
         lastUpdated: '更新时间', // string | boolean
